d55c6cd39f166efa6ce562e17b7e512ea9b0208e
b-istiak-s <istiakshovon001@gmail.com>
2024-10-16 12:26:17 +0600

    unnecessary libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,9 +46,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-    implementation("androidx.media3:media3-exoplayer:1.4.1")
-    implementation("androidx.media3:media3-exoplayer-dash:1.4.1")
-    implementation("androidx.media3:media3-ui:1.4.1")
-
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,6 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://jitpack.io") }
     }
 }
 dependencyResolutionManagement {
@@ -17,12 +16,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://jitpack.io") }
     }
-}
-
-gradle.extra.apply {
-    set("androidxMediaEnableMidiModule", true)
 }
 
 rootProject.name = "Study Helper"
